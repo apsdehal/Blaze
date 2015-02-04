53c212d046a10bea653fb507777a9128f5c58e6d
Amanpreet Singh <apsdehal@gmail.com>
2015-02-05 02:52:39 +0530

    chore(setup): Initial Commit

---
+++ lib/torrent/blaze.rb
@@ -0,0 +1,7 @@
+require "torrent/blaze/version"
+
+module Torrent
+  module Blaze
+    # Your code goes here...
+  end
+end
---
+++ lib/torrent/blaze/version.rb
@@ -0,0 +1,5 @@
+module Torrent
+  module Blaze
+    VERSION = "0.0.1"
+  end
+end
