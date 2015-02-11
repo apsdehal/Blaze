8cf6d28b95f2a4a3c93b49648e1d9eaead5cf6a5
Amanpreet Singh <apsdehal@gmail.com>
2015-02-12 00:19:00 +0530

    Add optparsing to CLI class

--- lib/blaze.rb
+++ lib/blaze.rb
@@ -1,7 +1,32 @@
-require "torrent/blaze/version"
+require "blaze/version"
+require "optparse"
+module Blaze
+	class CLI
+		def start
+			options = {}
+			opt_parse = OptionParser.new do |opt|
+				opt.banner = "Usage: opt_parser COMMAND [OPTIONS]"
+				opt.separator ""
+				opt.separator "Commands"
+				opt.separator "		torrent: magnet uri link"
+				opt.separator ""
+				opt.separator "Options"
 
-module Torrent
-  module Blaze
-    # Your code goes here...
-  end
+				opt.on("-t", "--torrent TORRENT", "Magnet uri link to a torrent") do |torrent|
+					options[:torrent] = torrent
+				end
+
+				opt.on("-o", "--output Download Directory", "Path to downaload directory") do |output|
+					options[:output] = output
+				end
+
+				opt.on("-h","--help","help") do
+					puts opt_parser
+				end
+
+				opt_parser.parse!
+			end
+
+		end
+	end
 end
